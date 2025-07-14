cbb1bf554f43833df1fd45509021f3a992515a8f
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-13 23:21:42 -0400

    action buttons workign

--- public/js/display/change-display.js
+++ public/js/display/change-display.js
@@ -13,14 +13,48 @@ export const changeActionButtonDisplay = async (clickElement) => {
   switch (clickElement.id) {
     case "get-updates-action-button":
       await unhideArray([d.updateIdListItem, d.commandListItem]);
-      break;
+      return true;
 
     case "send-message-action-button":
       await unhideArray([d.chatIdListItem, d.textInputListItem, d.commandListItem]);
-      break;
+      return true;
 
     case "forward-message-action-button":
+      await unhideArray([d.messageIdListItem, d.forwardFromListItem, d.forwardToListItem, d.commandListItem]);
+      return true;
+
+    case "forward-all-store-action-button":
+      await unhideArray([d.messageStartListItem, d.messageStopListItem, d.forwardFromListItem, d.forwardToListItem, d.forwardAllStoreTypeListItem, d.collectionSaveToListItem]);
+      d.commandInputElement.value = "forwardMessage";
+      d.collectionSaveToListItem.value = "";
+      return true;
+
+    case "edit-caption-action-button":
+      await unhideArray([d.messageIdListItem, d.editCaptionChannelListItem, d.textInputListItem, d.commandListItem]);
+      return true;
+
+    case "caption-all-lookup-action-button":
+      await unhideArray([
+        d.messageStartListItem,
+        d.messageStopListItem,
+        d.collectionPullFromListItem,
+        d.collectionSaveToListItem,
+        d.captionLookupTypeListItem,
+        d.editCaptionChannelListItem,
+        d.forwardToListItem,
+      ]);
+      d.commandInputElement.value = "editMessageCaption";
+      d.collectionPullFromElement.value = "";
+      d.collectionSaveToListItem.value = "";
+      return true;
+
+    case "upload-pics-action-button":
+      await unhideArray([d.uploadToListItem, d.uploadPicTypeListItem, d.picPathListItem, d.collectionPullFromListItem, d.commandListItem, d.collectionSaveToListItem]);
+      d.commandInputElement.value = "sendPhoto";
+      d.collectionPullFromElement.value = "";
+      d.collectionSaveToElement.value = "";
+      return true;
   }
 
-  return true;
+  return null;
 };
