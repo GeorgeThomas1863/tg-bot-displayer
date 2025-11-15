6df4afd1c47ad962d9aa01a1b4e0809f4c1fe82b
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-11-15 12:23:11 -0500

    dev

--- src/caption-all/caption-all-lookup.js
+++ src/caption-all/caption-all-lookup.js
@@ -3,38 +3,22 @@ import state from "../util/state.js";
 
 export const runCaptionAllLookup = async (inputParams) => {
   if (!inputParams || !state.active) return null;
-  const { captionAllType } = inputParams;
+  const { captionAllType, messageStart, messageStop, collectionPullFrom, collectionSaveTo, editChannelId, forwardToId } = inputParams;
 
   console.log("CAPTION ALL LOOKUP");
   console.log(inputParams);
-
-  switch (captionAllType) {
-    case "setToFileName":
-      return await runSetToFileName(inputParams);
-    case "lookupFileName":
-      return await runLookupFileName(inputParams);
-    case "lookupSpecial":
-      return await runLookupSpecial(inputParams);
-    case "clearVidCaptions":
-      return await runClearVidCaptions(inputParams);
-  }
-};
-
-export const runSetToFileName = async (inputParams) => {
-  if (!state.active) return null;
-  const { collectionPullFrom, collectionSaveTo, messageStart, messageStop, editChannelId, forwardToId } = inputParams;
 
   const returnDataArray = [];
   for (let i = messageStart; i < messageStop; i++) {
     if (!state.active) return null;
     try {
-      const params = {
+      const forwardParams = {
         forwardToId: forwardToId,
         forwardFromId: editChannelId,
         messageId: i,
       };
 
-      const forwardData = await tgForwardMessage(params);
+      const forwardData = await tgForwardMessage(forwardParams);
       if (!forwardData) continue;
       console.log("FORWARD DATA");
       console.log(forwardData);
@@ -42,6 +26,41 @@ export const runSetToFileName = async (inputParams) => {
       console.log(e.message + "\n" + e.data + "\n" + e.status);
     }
   }
+
+  // switch (captionAllType) {
+  //   case "setToFileName":
+  //     return await runSetToFileName(inputParams);
+  //   case "lookupFileName":
+  //     return await runLookupFileName(inputParams);
+  //   case "lookupSpecial":
+  //     return await runLookupSpecial(inputParams);
+  //   case "clearVidCaptions":
+  //     return await runClearVidCaptions(inputParams);
+  // }
+};
+
+export const runSetToFileName = async (inputParams) => {
+  if (!state.active) return null;
+  // const { collectionPullFrom, collectionSaveTo, messageStart, messageStop, editChannelId, forwardToId } = inputParams;
+
+  // const returnDataArray = [];
+  // for (let i = messageStart; i < messageStop; i++) {
+  //   if (!state.active) return null;
+  //   try {
+  //     const params = {
+  //       forwardToId: forwardToId,
+  //       forwardFromId: editChannelId,
+  //       messageId: i,
+  //     };
+
+  //     const forwardData = await tgForwardMessage(params);
+  //     if (!forwardData) continue;
+  //     console.log("FORWARD DATA");
+  //     console.log(forwardData);
+  //   } catch (e) {
+  //     console.log(e.message + "\n" + e.data + "\n" + e.status);
+  //   }
+  // }
 };
 
 export const runLookupFileName = async (inputParams) => {
