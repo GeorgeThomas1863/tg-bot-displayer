e6f3058fe6c1db76b4cb4c4eb845494d714187a7
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-11-15 22:34:43 -0500

    forward all dev

--- src/forward-all/forward-all-store.js
+++ src/forward-all/forward-all-store.js
@@ -1,6 +1,6 @@
 import dbModel from "../../models/db-model.js";
 import { tgForwardMessage } from "../tg-api.js";
-// import { buildVidParams, buildEverythingParams, buildTextParams } from "../util/params-back.js";
+import { buildVidParams, buildPicParams, buildTextParams } from "../util/params-back.js";
 import state from "../util/state.js";
 
 export const runForwardAllStore = async (inputParams) => {
@@ -102,85 +102,6 @@ export const storeBlanksObj = async (forwardData) => {
   const { result } = forwardData;
   if (!result.caption || result.caption === "") return await buildVidParams(result);
   return null;
-};
-
-export const buildVidParams = async (inputObj) => {
-  if (!inputObj || !inputObj.video) return null;
-  const { video, message_id, forward_from_message_id, forward_from_chat, chat, caption, date, forward_date } = inputObj;
-  const { file_id, file_unique_id, file_name, file_size, duration } = video;
-
-  const vidParams = {
-    messageId: message_id,
-    forwardFromMessageId: forward_from_message_id,
-    forwardFromChannelId: forward_from_chat.id,
-    forwardFromChannelName: forward_from_chat.title,
-    forwardToId: chat.id,
-    forwardToName: chat.title,
-    fileFullId: file_id,
-    fileUniqueId: file_unique_id,
-    fileName: file_name,
-    fileSize: file_size,
-    videoLength: duration,
-    caption: caption,
-    datePosted: forward_date,
-    dateForwarded: date,
-    dateStored: new Date(),
-    paramType: "vidParams",
-  };
-
-  return vidParams;
-};
-
-export const buildPicParams = async (inputObj) => {
-  if (!inputObj || !inputObj.photo) return null;
-  const { photo, message_id, forward_from_message_id, forward_from_chat, chat, caption, date, forward_date } = inputObj;
-
-  //get input photo array length
-  const k = photo.length - 1;
-  const { file_id, file_unique_id, file_name, file_size, width, height } = photo[k];
-
-  const picParams = {
-    messageId: message_id,
-    forwardFromMessageId: forward_from_message_id,
-    forwardFromChannelId: forward_from_chat.id,
-    forwardFromChannelName: forward_from_chat.title,
-    forwardToId: chat.id,
-    forwardToName: chat.title,
-    fileFullId: file_id,
-    fileUniqueId: file_unique_id,
-    fileName: file_name,
-    fileSize: file_size,
-    picWidth: width,
-    picHeight: height,
-    caption: caption,
-    datePosted: forward_date,
-    dateForwarded: date,
-    dateStored: new Date(),
-    paramType: "picParams",
-  };
-
-  return picParams;
-};
-
-export const buildTextParams = async (inputObj) => {
-  if (!inputObj || !inputObj.text) return null;
-  const { text, message_id, forward_from_message_id, forward_from_chat, chat, date, forward_date } = inputObj;
-
-  const textParams = {
-    messageId: message_id,
-    forwardFromMessageId: forward_from_message_id,
-    forwardFromChannelId: forward_from_chat.id,
-    forwardFromChannelName: forward_from_chat.title,
-    forwardToId: chat.id,
-    forwardToName: chat.title,
-    text: text,
-    datePosted: forward_date,
-    dateForwarded: date,
-    dateStored: new Date(),
-    paramType: "textParams",
-  };
-
-  return textParams;
 };
 
 // export const parseStoreParams = async (inputData, forwardAllType, dataType) => {
--- src/util/params-back.js
+++ src/util/params-back.js
@@ -1,3 +1,82 @@
+export const buildVidParams = async (inputObj) => {
+  if (!inputObj || !inputObj.video) return null;
+  const { video, message_id, forward_from_message_id, forward_from_chat, chat, caption, date, forward_date } = inputObj;
+  const { file_id, file_unique_id, file_name, file_size, duration } = video;
+
+  const vidParams = {
+    messageId: message_id,
+    forwardFromMessageId: forward_from_message_id,
+    forwardFromChannelId: forward_from_chat.id,
+    forwardFromChannelName: forward_from_chat.title,
+    forwardToId: chat.id,
+    forwardToName: chat.title,
+    fileFullId: file_id,
+    fileUniqueId: file_unique_id,
+    fileName: file_name,
+    fileSize: file_size,
+    videoLength: duration,
+    caption: caption,
+    datePosted: forward_date,
+    dateForwarded: date,
+    dateStored: new Date(),
+    paramType: "vidParams",
+  };
+
+  return vidParams;
+};
+
+export const buildPicParams = async (inputObj) => {
+  if (!inputObj || !inputObj.photo) return null;
+  const { photo, message_id, forward_from_message_id, forward_from_chat, chat, caption, date, forward_date } = inputObj;
+
+  //get input photo array length
+  const k = photo.length - 1;
+  const { file_id, file_unique_id, file_name, file_size, width, height } = photo[k];
+
+  const picParams = {
+    messageId: message_id,
+    forwardFromMessageId: forward_from_message_id,
+    forwardFromChannelId: forward_from_chat.id,
+    forwardFromChannelName: forward_from_chat.title,
+    forwardToId: chat.id,
+    forwardToName: chat.title,
+    fileFullId: file_id,
+    fileUniqueId: file_unique_id,
+    fileName: file_name,
+    fileSize: file_size,
+    picWidth: width,
+    picHeight: height,
+    caption: caption,
+    datePosted: forward_date,
+    dateForwarded: date,
+    dateStored: new Date(),
+    paramType: "picParams",
+  };
+
+  return picParams;
+};
+
+export const buildTextParams = async (inputObj) => {
+  if (!inputObj || !inputObj.text) return null;
+  const { text, message_id, forward_from_message_id, forward_from_chat, chat, date, forward_date } = inputObj;
+
+  const textParams = {
+    messageId: message_id,
+    forwardFromMessageId: forward_from_message_id,
+    forwardFromChannelId: forward_from_chat.id,
+    forwardFromChannelName: forward_from_chat.title,
+    forwardToId: chat.id,
+    forwardToName: chat.title,
+    text: text,
+    datePosted: forward_date,
+    dateForwarded: date,
+    dateStored: new Date(),
+    paramType: "textParams",
+  };
+
+  return textParams;
+};
+
 // import state from "./state.js";
 
 // export const buildEverythingParams = async (inputParams, dataType = null) => {
