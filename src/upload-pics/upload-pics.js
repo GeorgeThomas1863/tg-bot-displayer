efa1795a5679137eaae2abef263b8d161517690a
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-11-19 20:12:42 -0500

    dev

--- src/upload-pics/upload-pics.js
+++ src/upload-pics/upload-pics.js
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import state from "../util/state.js";
 
 import { tgPostPicFS } from "../tg-api.js";
@@ -6,20 +8,75 @@ export const runUploadPics = async (inputParams) => {
   if (!inputParams || !state.active) return null;
   const { uploadPicType, uploadToId, picPath } = inputParams;
 
-  console.log("RUN UPLOAD PICS");
-  console.log(inputParams);
+  if (uploadPicType === "uploadSingleFS" || uploadPicType === "uploadFolderFS") return await uploadPicsFS(inputParams);
 
-  const params = {
-    chatId: uploadToId,
-    picPath: picPath,
-  };
+  //   console.log("RUN UPLOAD PICS");
+  //   console.log(inputParams);
 
-  const data = await tgPostPicFS(params);
-  console.log("DATA");
-  console.log(data);
-  return data;
+  //   const params = {
+  //     chatId: uploadToId,
+  //     picPath: picPath,
+  //   };
+
+  //   const data = await tgPostPicFS(params);
+  //   console.log("DATA");
+  //   console.log(data);
+  //   return data;
 };
 
-// export const uploadPicFS = async (inputParams) => {
-//     if (!inputParams || !state.active) return null;
-// }
+export const uploadPicsFS = async (inputParams) => {
+  if (!inputParams || !state.active) return null;
+  const { uploadToId } = inputParams;
+
+  const uploadPicArray = await getUploadPicArray(inputParams);
+  if (!uploadPicArray) return null;
+
+  const postPicDataArray = [];
+  for (let i = 0; i < uploadPicArray.length; i++) {
+    if (!state.active) return null;
+    const picPath = uploadPicArray[i];
+    const params = {
+      chatId: uploadToId,
+      picPath: picPath,
+    };
+    const data = await tgPostPicFS(params);
+    if (!data) continue;
+    postPicDataArray.push(data);
+  }
+
+  return postPicDataArray;
+};
+
+export const getUploadPicArray = async (inputParams) => {
+  if (!inputParams || !state.active) return null;
+  const { uploadPicType } = inputParams;
+  let picPath = inputParams.picPath;
+
+  if (uploadPicType === "uploadSingleFS") {
+    if (!fs.existsSync(picPath)) {
+      console.log(`PIC PATH DOES NOT EXIST: ${picPath}`);
+      return null;
+    }
+    return [picPath];
+  }
+
+  if (uploadPicType !== "uploadFolderFS") return null;
+
+  if (picPath.endsWith("/")) picPath = picPath.slice(0, -1);
+  if (!fs.existsSync(picPath)) {
+    console.log(`PIC PATH DOES NOT EXIST: ${picPath}`);
+    return null;
+  }
+
+  //build folder
+  const uploadPicArray = [];
+  const folder = fs.readdirSync(picPath);
+  for (let i = 0; i < folder.length; i++) {
+    const file = folder[i];
+    const filePath = path.join(picPath, file);
+    if (!fs.statSync(filePath).isFile()) continue;
+    uploadPicArray.push(filePath);
+  }
+
+  return uploadPicArray;
+};
